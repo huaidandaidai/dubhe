4d2743d50e5874aeb1997886f4884fb5f48dae13
lishanhui <1004928908@qq.com>
2019-10-23 23:23:31 +0800

    数据库表设计

--- src/main/resources/init_database.sql
+++ src/main/resources/init_database.sql
@@ -8,7 +8,7 @@ CREATE TABLE dubhe_user (
   user_password VARCHAR (50) DEFAULT NULL,
   user_name VARCHAR (100) DEFAULT NULL,
   delete_flag INT(1) DEFAULT NULL,
-  revison INT (32) DEFAULT NULL,
+  version INT (32) DEFAULT NULL,
   create_datetime datetime DEFAULT NULL,
   create_user_id BIGINT (11) DEFAULT NULL,
   create_departed_id BIGINT (11) DEFAULT NULL,
@@ -17,5 +17,21 @@ CREATE TABLE dubhe_user (
   update_departed_id BIGINT (11) DEFAULT NULL,
   PRIMARY KEY (user_id)
 ) ENGINE = INNODB DEFAULT CHARSET = utf8;
-insert into dubhe_user(user_id,user_account,user_password,user_name,delete_flag,revison) values(1,'admin','123456','管理员','0','0');
+insert into dubhe_user(user_id,user_account,user_password,user_name,delete_flag,version) values(1,'admin','123456','管理员','0','0');
+
+DROP TABLE IF EXISTS dubhe_user;
+CREATE TABLE dubhe_depart (
+  depart_id BIGINT (11) NOT NULL ,
+  depart_name VARCHAR (100) DEFAULT NULL,
+  partent_depart_id BIGINT (11) NOT NULL ,
+  delete_flag INT(1) DEFAULT NULL,
+  version INT (32) DEFAULT NULL,
+  create_datetime datetime DEFAULT NULL,
+  create_user_id BIGINT (11) DEFAULT NULL,
+  create_departed_id BIGINT (11) DEFAULT NULL,
+  update_datetime datetime DEFAULT NULL,
+  update_user_id BIGINT (11) DEFAULT NULL,
+  update_departed_id BIGINT (11) DEFAULT NULL,
+  PRIMARY KEY (user_id)
+) ENGINE = INNODB DEFAULT CHARSET = utf8;
 
